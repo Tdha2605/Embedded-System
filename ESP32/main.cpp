642afc408a70b19bd6a1bc50355fa2a9dde3a6dc
THanh03 <109448448+Tdha2605@users.noreply.github.com>
2024-06-14 08:08:40 +0700

    Update main.cpp

--- ESP32/main.cpp
+++ ESP32/main.cpp
@@ -1,85 +1,45 @@
 #include "esp_camera.h"
-#include "Arduino.h"
-#include "FS.h"               // SD Card ESP32
-#include "SD_MMC.h"           // SD Card ESP32
-#include "soc/soc.h"          // Disable brownout problems
-#include "soc/rtc_cntl_reg.h" // Disable brownout problems
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
 #include "driver/rtc_io.h"
-#include <EEPROM.h> // read and write from flash memory
-#include "esp_timer.h"
 #include <Wire.h>
-#include "RTClib.h" // Library for RTC
-#include "SPI.h"
-
-// define the number of bytes you want to access
-#define EEPROM_SIZE 1
+#include <RTClib.h>
 
 // Pin definition for CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
+// I2C pins for DS1307
+#define SDA_PIN 14
+#define SCL_PIN 15
 
-#define BUTTON_PIN 13 // Define the button pin
-#define I2C_SDA 4     // Define the I2C SDA pin
-#define I2C_SCL 15    // Define the I2C SCL pin
+// LED pin
+#define LED_PIN 4
 
-int pictureNumber = 0;
-bool take_picture_now = false;
-volatile bool buttonPressed = false;
-volatile unsigned long lastPressTime = 0;
-const unsigned long doublePressInterval = 500; // Interval in milliseconds
+// RTC instance
+RTC_DS1307 rtc;
 
-hw_timer_t *timer = NULL;
-RTC_DS1307 rtc; // Create an RTC object
+// Stores the camera configuration parameters
+camera_config_t config;
 
-void IRAM_ATTR onTimer()
-{
-  take_picture_now = true;
-}
-
-void IRAM_ATTR onButtonPress()
-{
-  unsigned long currentTime = millis();
-  if (currentTime - lastPressTime < doublePressInterval)
-  {
-    // Double press detected, reset EEPROM
-    for (int i = 0; i < EEPROM_SIZE; i++)
-    {
-      EEPROM.write(i, 0);
-    }
-    EEPROM.commit();
-    Serial.println("EEPROM has been reset.");
-    pictureNumber = 0;
-  }
-  else
-  {
-    // Single press detected, set flag to take picture
-    buttonPressed = true;
-  }
-  lastPressTime = currentTime;
-}
-
-void setup()
-{
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
-
-  Serial.begin(115200);
-
-  camera_config_t config;
+// Initializes the camera
+void configInitCamera() {
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
   config.pin_d0 = Y2_GPIO_NUM;
@@ -94,134 +54,130 @@ void setup()
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
   config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
+  config.pixel_format = PIXFORMAT_JPEG; //YUV422,GRAYSCALE,RGB565,JPEG
+  config.grab_mode = CAMERA_GRAB_LATEST;
 
-  if (psramFound())
-  {
+  // Select lower framesize if the camera doesn't support PSRAM
+  if (psramFound()) {
     config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  }
-  else
-  {
+    config.jpeg_quality = 10; //0-63 lower number means higher quality
+    config.fb_count = 1;
+  } else {
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
 
-  // Init Camera
+  // Initialize the Camera
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK)
-  {
+  if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
+}
 
-  // Init SD Card
-  if (!SD_MMC.begin())
-  {
+// Get the picture filename based on the current time from the RTC
+String getPictureFilename() {
+  DateTime now = rtc.now();
+  char timeString[20];
+  snprintf(timeString, sizeof(timeString), "%04d-%02d-%02d_%02d-%02d-%02d",
+           now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+  Serial.println(timeString);
+  String filename = "/picture_" + String(timeString) + ".jpg";
+  return filename;
+}
+
+// Initialize the micro SD card
+void initMicroSDCard() {
+  // Start Micro SD card
+  Serial.println("Starting SD Card");
+  if (!SD_MMC.begin()) {
     Serial.println("SD Card Mount Failed");
     return;
   }
-
+  delay(1000);
   uint8_t cardType = SD_MMC.cardType();
-  if (cardType == CARD_NONE)
-  {
+  if (cardType == CARD_NONE) {
     Serial.println("No SD Card attached");
     return;
   }
-
-  // Initialize EEPROM with predefined size
-  EEPROM.begin(EEPROM_SIZE);
-  pictureNumber = EEPROM.read(0) + 1;
-
-  // Set up button pin
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), onButtonPress, FALLING);
-
-  // Set up timer to trigger every 30 seconds
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 30000000, true);
-  timerAlarmEnable(timer);
-
-  // Initialize RTC
-  Wire.begin(I2C_SDA, I2C_SCL); // SDA, SCL
-  if (!rtc.begin())
-  {
-    Serial.println("Couldn't find RTC");
-    while (1)
-      ;
-  }
-
-  if (!rtc.isrunning())
-  {
-    Serial.println("RTC is NOT running, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
-
-  // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-  rtc_gpio_hold_en(GPIO_NUM_4);
 }
 
-void takePicture()
-{
-  camera_fb_t *fb = NULL;
+// Take photo and save to microSD card
+void takeSavePhoto() {
+  // Turn on the LED
+  digitalWrite(LED_PIN, HIGH);
 
   // Take Picture with Camera
-  fb = esp_camera_fb_get();
-  if (!fb)
-  {
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
     Serial.println("Camera capture failed");
-    return;
+    delay(1000);
+    ESP.restart();
   }
 
-  DateTime now = rtc.now();
-  String path = "/picture_" + String(now.year()) + "_" + String(now.month()) + "_" + String(now.day()) + "_" +
-                String(now.hour()) + "_" + String(now.minute()) + "_" + String(now.second()) + ".jpg";
-
-  fs::FS &fs = SD_MMC;
+  // Path where new picture will be saved in SD Card
+  String path = getPictureFilename();
   Serial.printf("Picture file name: %s\n", path.c_str());
 
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC;
   File file = fs.open(path.c_str(), FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file in writing mode");
-  }
-  else
-  {
+  if (!file) {
+    Serial.printf("Failed to open file in writing mode");
+  } else {
     file.write(fb->buf, fb->len); // payload (image), payload length
-    Serial.printf("Saved file to path: %s\n", path.c_str());
-    EEPROM.write(0, pictureNumber);
-    EEPROM.commit();
-    pictureNumber++;
+    Serial.printf("Saved: %s\n", path.c_str());
   }
   file.close();
   esp_camera_fb_return(fb);
+
+  // Turn off the LED
+  digitalWrite(LED_PIN, LOW);
 }
 
-void loop()
-{
-  if (take_picture_now)
-  {
-    take_picture_now = false;
-    takePicture();
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+
+  Serial.begin(115200);
+  delay(2000);
+
+  // Initialize I2C communication for the RTC
+  Wire.begin(SDA_PIN, SCL_PIN); // SDA, SCL
+
+  // Initialize RTC
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
   }
 
-  if (buttonPressed)
-  {
-    buttonPressed = false;
-    takePicture();
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // Set the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  delay(100); // Small delay to avoid watchdog timer issues
+  // Initialize the camera
+  Serial.print("Initializing the camera module...");
+  configInitCamera();
+  Serial.println("Ok!");
+  
+  // Initialize MicroSD
+  Serial.print("Initializing the MicroSD card module... ");
+  initMicroSDCard();
+
+  // Initialize LED pin
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Ensure LED is off initially
+}
+
+void loop() {
+  // Take and Save Photo
+  takeSavePhoto();
+  delay(10000);
 }
